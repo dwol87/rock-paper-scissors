f9e2ada37208568242edfcb84bcdbc4460f6568d
David Wolinsky <dwolinsky87@gmail.com>
2021-11-24 18:10:15 -0500

    11/24/2021

--- script.js
+++ script.js
@@ -4,3 +4,24 @@ function computerPlay() {
   return myArray[~~(Math.random() * myArray.length)];
 }
 
+let playerSelection = "Rock";
+let computerSelection = computerPlay();
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == "Rock" && computerSelection == "Scissors") {
+        return `You Lose! Paper beats Rock`
+    }
+    if (playerSelection == "Rock" && computerSelection == "Paper") {
+        return `You Win! Rock beats Paper`
+    } else {
+        return `You Tied`
+    }
+}
+
+/* Needs Work Below*/
+
+function game() {
+    return playRound(playerSelection, computerSelection);
+}
+
+console.log(game())
