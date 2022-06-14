57301bc48b423c1b2c0fddca19baf916a6285cec
David Wolinsky <dwolinsky87@gmail.com>
2022-06-13 22:01:37 -0400

    Ugh

--- script.js
+++ script.js
@@ -4,10 +4,9 @@ function computerPlay() {
     return compChoice[~~(Math.random() * compChoice.length)];
 };
 
-let playerSelection = prompt('Rock, Paper, or Scissors?');
+let playerSelection = prompt('Rock, Paper, or Scissors?').toLowerCase();
 let computerSelection = computerPlay().toLowerCase();
-console.log(playerSelection)
-console.log(computerSelection)
+
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection == computerSelection) {
@@ -15,14 +14,31 @@ function playRound(playerSelection, computerSelection) {
     } else if 
      (playerSelection == "rock" && computerSelection == "scissors" ||
       playerSelection == "paper" && computerSelection == "rock" ||
-      playerSelection == "scissors" && computerSelection == "paper") {   
+      playerSelection == "scissors" && computerSelection == "paper") {  
         return `Player Wins!`
     } else if 
      (playerSelection == "rock" && computerSelection == "paper" ||
       playerSelection == "paper" && computerSelection == "scissors" || 
       playerSelection == "scissors" && computerSelection == "rock") {  
-    }   return `You Lose!` 
+    }   return `Computers Wins!` 
 };
-console.log(playRound(playerSelection, computerSelection));
+
+let roundResults = playRound(playerSelection, computerSelection);
+let computerScore = 0;
+let playerScore = 0;
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        playRound(playerSelection, computerSelection);
+        console.log(playerSelection)
+        console.log(computerSelection)
+        return roundResults;
+    }
+};
+
+
+console.log(game());
+
+
 
 
